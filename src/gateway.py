881ed69fa262e796ade9bf65d4010a262f7433b3
22preich <44716533+22preich@users.noreply.github.com>
2019-12-04 18:33:36 -0800

    login to discord

---
+++ src/gateway.py
@@ -0,0 +1,130 @@
+import websockets
+import asyncio
+import json
+
+
+class BaseGateway:
+
+    def __init__(self):
+        self.connected = False
+
+    def refresh(self):
+        pass
+
+    def open(self):
+        pass
+
+    def close(self):
+        pass
+
+
+class APIGateway(BaseGateway):
+
+    def __init__(self):
+        super(APIGateway, self).__init__()
+        self.token = None
+
+    def refresh(self):
+        pass
+
+    def open(self):
+        pass
+
+    def close(self):
+        pass
+
+
+class WebsocketAPIGateway(APIGateway):
+
+    def __init__(self):
+        super(APIGateway, self).__init__()
+        self.connection = None
+
+    def refresh(self):
+        pass
+
+    def open(self):
+        pass
+
+    def close(self):
+        pass
+
+
+class DiscordGateway(WebsocketAPIGateway):
+    DISPATCH = 0
+    HEARTBEAT = 1
+    IDENTIFY = 2
+    PRESENCE = 3
+    VOICE_STATE = 4
+    VOICE_PING = 5
+    RESUME = 6
+    RECONNECT = 7
+    REQUEST_MEMBERS = 8
+    INVALIDATE_SESSION = 9
+    HELLO = 10
+    HEARTBEAT_ACK = 11
+    GUILD_SYNC = 12
+
+    def __init__(self):
+        super(WebsocketAPIGateway, self).__init__()
+        self.token = "NTUzODc2MzA0NDIwNjY3Mzky.D2UdAQ._ABnKKMRzGyn9aHrJoA7E5Hd1ZU"
+        self.heartbeak_interval = None
+        self.loop = asyncio.get_event_loop()
+        self.dispatch_map = {
+            "READY"
+        }
+
+    async def open(self):
+        self.connection = await websockets.connect("wss://gateway.discord.gg/?v=6&encoding=json")
+        response = json.loads(await self.connection.recv())
+        if response["op"] == self.HELLO:
+            print("opened connection to discord", response)
+        self.heartbeak_interval = response["d"]["heartbeat_interval"]
+
+    async def send_raw(self, data):
+        await self.connection.send(data)
+
+    async def send(self, op, s=None, t=None, d=None):
+        print("sent packet")
+        await self.send_raw(json.dumps(dict(op=op, s=s, t=t, d=d)))
+
+    async def accept_dispatch(self):
+        while True:
+            response = await self.connection.recv()
+            response = json.loads(response)
+
+
+    async def heartbeat(self):
+        while True:
+            await self.send(op=self.HEARTBEAT)
+            print(self.heartbeak_interval/1000)
+            await asyncio.sleep(self.heartbeak_interval/1000, loop=self.loop)
+
+    async def start_heartbeat(self):
+        self.loop.create_task(self.heartbeat())
+        # self.loop.run_forever()
+
+    async def login(self):
+        login_str = {
+            "token": self.token,
+            "properties": {
+                "$os": "windows",
+                "$browser": "chrome",
+                "$device": "disco"
+            },
+            "presence": {
+                "game": {
+                    "name": "Cards Against Humanity",
+                    "type": 0
+                },
+            }
+        }
+        await self.send(op=self.IDENTIFY, d=login_str)
+        print(await self.connection.recv())
+
+    async def close(self):
+        await self.connection.close()
+
+    @classmethod
+    def sample_hearbeat(cls):
+        return json.dumps(dict(op=cls.HEARTBEAT))
---
+++ src/main.py
@@ -0,0 +1,24 @@
+import asyncio
+from gateway import DiscordGateway
+
+loop = asyncio.new_event_loop()
+
+
+async def constant_update(func):
+    while True:
+        await func()
+        await asyncio.sleep(3, loop=loop)
+
+
+async def main():
+    discord = DiscordGateway()
+    await discord.open()
+    await discord.login()
+    await discord.start_heartbeat()
+    try:
+        await asyncio.sleep(100)
+    finally:
+        await discord.close()
+        exit()
+
+asyncio.run(main())
